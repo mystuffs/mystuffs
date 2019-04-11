ade43f8a17928911945b4f955a96c90ecf00daa9
Michelle w <michwong10@gmail.com>
2019-04-11 17:59:33 -0400

    fixed some css issues
    fixed layout

--- js/additem.js
+++ js/additem.js
@@ -3,8 +3,3 @@ let itemInput=document.getElementByClass("field-wrap");
 
 let addButton=document.getElementsByClass("add-button");
 
-//New task list item
-let createNewItemListing=function(taskString)
-{
-
-    let listItem=document.createElement("li");
