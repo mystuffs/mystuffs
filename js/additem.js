97fb798de4720218f7c066a57477bd12f6aff2de
Michelle w <michwong10@gmail.com>
2019-04-11 16:38:02 -0400

    added jv let variables
    jv edits

--- js/additem.js
+++ js/additem.js
@@ -1,1 +1,10 @@
-let itemInput=document.getElementById("field-wrap");
+//Add a new item.
+let itemInput=document.getElementByClass("field-wrap");
+
+let addButton=document.getElementsByClass("add-button");
+
+//New task list item
+let createNewItemListing=function(taskString)
+{
+
+    let listItem=document.createElement("li");
