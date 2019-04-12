48228a8d4a550f0fb70599b96a2ba7be082e4e50
Michelle Wong <michwong10@gmail.com>
2019-04-12 16:00:45 -0400

    fixed css

--- js/additem.js
+++ js/additem.js
@@ -1,5 +1,33 @@
 //Add a new item.
-let itemInput=document.getElementByClass("field-wrap");
+// let itemInput=document.getElementByClass("field-wrap");
 
-let addButton=document.getElementsByClass("add-button");
+// let addButton=document.getElementsByClass("add-button");
 
+constant newItem = document.getElementsById('form-wrapper');
+
+//Array  
+
+let addItem; 
+
+addItem = [
+    {name:"Spoon", 
+            price:"Priceless", 
+            location:"Miami", 
+            condition:"Awful", 
+            category:"Kitchen", 
+            description:"Truly amazing, how elegant!"},
+
+            {name:"Spoon2", 
+            price:"Priceless", 
+            location:"Miami", 
+            condition:"Awful", 
+            category:"Kitchen", 
+            description:"Truly amazing, how elegant!"},
+
+            {name:"Spoon3", 
+            price:"Priceless", 
+            location:"Miami", 
+            condition:"Awful", 
+            category:"Kitchen", 
+            description:"Truly amazing, how elegant!"},
+];
