62a1fa44310d5117f55897788736177596e3e22d
Tyler Lovell <tylerlovell@gmail.com>
2019-04-18 19:43:41 -0400

    javascript update and error testing

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-const itemspage = document.getElementById(`itemlist`);
+const itemspage = document.getElementById(`itmlst`);
 const newitemform = document.getElementById(`add-content`);
 const addItemsButton = document.getElementById(`add-button`);
 
-
-// Array
-let itemList;
-
 // testing array use
-itemList = [
+const itemList = [
             {name:"Spoon", 
             price:"Priceless", 
             location:"Miami", 
@@ -31,51 +27,50 @@ itemList = [
             ];
 
 
-// GENERIC PRINTER 
-// Will clear out the current list to replace with a new list
-function printList(theArrayToPrint = itemList) {
+let printListing = (listing) => {
 
-    // Add list of <li> items to the document
-    // Replace these with necessary array of items and values 
-    itemspage.innerHTML = theArrayToPrint.map(item => `
-        <li class="item">
+    return `
+            <li class="item">
             <input type="radio" name="items">
-                <div class="name">${item.name}</div>
-                <div class="price">${item.price}</div>
-                <div class="location">${item.location}</div>
-                <div class="condition">${item.condition}</div>
-                <div class="category">${item.category}</div>
-                <div class="description">${item.description}</div>
-         </li>
-    `).join('');
-}
+                <div class="name">${listing.name}</div>
+                <div class="price">${listing.price}</div>
+                <div class="location">${listing.location}</div>
+                <div class="condition">${listing.condition}</div>
+                <div class="category">${listing.category}</div>
+                <div class="description">${listing.description}</div>
+            </li>`;
+ }
+ 
+ let printAllEvents = () => {
+    itemspage.innerHTML += itemList.map(printListing).join('');
+ }
+ 
+ printAllEvents();
+
 
 
 // STARTUP THE APPLICATION ///////////////////////////////////////////
 // When the window is loaded, start the application!
-window.addEventListener('load', event => {
 
-    itemList = JSON.parse(window.localStorage.getItem('shoppingList')) || [];
-   
-    printList();
-
-});
 
 // ADD A NEW ITEM //////////////////////////////////////////////
 // When the form is submit, take the field and add to the list
-addItemsButton.addEventListener('submit', event => {
+//addItemsButton.addEventListener('submit', event => {
 
      // submit form and no refresh page
     
-    event.preventDefault();
+    //event.preventDefault();
 
     // Get the text from the field
-    let newitemform = newitemform.item.value;
+   // let newitemform = newitemform.item.value;
  //clear
-    newitemform.item.value = '';
+   // newitemform.item.value = '';
 
-    newitemform.item.focus();
+   // newitemform.item.focus();
 
-    itemList.push( { id: shoppingList.name, price, location, condition: category, description } );
-});
+    //itemList.push( { id: shoppingList.name, price, location, condition: category, description } );
 //trying to add info into the form and clear form- information in the form should be submitted and generated onto the next page
+
+
+
+
